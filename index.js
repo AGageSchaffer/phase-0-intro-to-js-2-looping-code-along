2faca16c183da3d853a5f69a468998cd5b73b321
AustinSchaffer <austinschaffer@gmail.com>
2022-12-15 12:34:23 -0600

    commit tree bd377cf743192277ec651428991ac7c737d09b2a

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(cards) {
+    const newCards = []
+    for (let i = 0; i < cards.length; i++)
+    newCards.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`) 
+    return newCards
+}
+writeCards()
+
+function countDown() {
+    for (let countDown = 10; countDown >= 0; countDown--) {
+    console.log(countDown)}
+    return countDown
+}
